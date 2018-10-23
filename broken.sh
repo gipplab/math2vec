fd67e20623ede00a90841651f4dc02190474465d
AndreG-P <andre.greiner-petter@t-online.de>
2018-10-24 03:50:03 +0900

    Add a broken script to filter broken conversion files

--- broken.sh
+++ broken.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-PATH="/home/andreg-p/arxmliv/no_problems_txt"
+INPUT="/home/andreg-p/arxmliv/no_problems_txt"
 RAW="/home/andreg-p/arxmliv/no_problems_raw"
 OUTPUT="/home/andreg-p/arxmliv/no_problems_broken"
 
-# requesting all files that are smaller than 10 bytes -> most likely broken
-for file in $(find $PATH/ -maxdepth 1 -type f -size -10c); do
+echo "Identify files smaller than 40 bytes."
+
+# requesting all files that are smaller than 40 bytes -> most likely broken
+files=$(find $INPUT/ -maxdepth 1 -type f -size -40c -printf "%P ")
+
+echo "Identified files... start moving them to $OUTPUT"
+
+for file in $files; do
 	# get the name without path and file extensions
 	name=${file%.*}
-	echo "Found a file $name"
+	$(mv $INPUT/$name.* $OUTPUT)
+	$(mv $RAW/$name.* $OUTPUT)
 done
+
+echo "Process finished"
