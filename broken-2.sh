000b2bc861998afc7706a3f7e80a232cfbc40fe7
AndreG-P <andre.greiner-petter@t-online.de>
2018-10-24 19:37:01 +0900

    Update planetext config and organize to rerun broken files

---
+++ broken-2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+INPUT="errors.txt"
+RAW="/home/andreg-p/arxmliv/no_problems_raw"
+OUTPUT="/home/andreg-p/arxmliv/no_problems_tmp"
+
+files=$(cat $INPUT)
+
+for file in $files; do
+	$(cp $RAW/$file $OUTPUT/$file)
+done
