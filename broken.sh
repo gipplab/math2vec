9d0fdbfbdece270bf1b91d8dc7982ae55abb20b5
AndreG-P <andre.greiner-petter@t-online.de>
2018-10-25 14:50:19 +0900

    Minor changes

--- broken.sh
+++ broken.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-INPUT="/home/andreg-p/arxmliv/no_problems_txt"
+INPUT="/home/andreg-p/arxmliv/no_problems_train"
+
+TXT="/home/andreg-p/arxmliv/no_problems_txt"
 RAW="/home/andreg-p/arxmliv/no_problems_raw"
 OUTPUT="/home/andreg-p/arxmliv/no_problems_broken"
 
@@ -9,13 +11,14 @@ echo "Identify files smaller than 40 bytes."
 # requesting all files that are smaller than 40 bytes -> most likely broken
 files=$(find $INPUT/ -maxdepth 1 -type f -size -40c -printf "%P ")
 
-echo "Identified files... start moving them to $OUTPUT"
+echo "Identified files... start moving them to $OUTPUT/"
 
 for file in $files; do
 	# get the name without path and file extensions
 	name=${file%.*}
-	$(mv $INPUT/$name.* $OUTPUT)
-	$(mv $RAW/$name.* $OUTPUT)
+	$(mv $INPUT/$name.txt $OUTPUT/$name.txt)
+	$(mv $RAW/$name.html $OUTPUT/$name.html)
+	$(mv $TXT/$name.* $OUTPUT/txt/)
 done
 
 echo "Process finished"
