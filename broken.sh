a00bfc02c545018b3fd0be6ffccc040dc7b7dc47
AndreG-P <andre.greiner-petter@t-online.de>
2018-10-30 02:49:27 +0900

    Reset Gemfile.lock for NII server version and add a new example to data/html

--- broken.sh
+++ broken.sh
@@ -9,15 +9,19 @@ OUTPUT="/home/andreg-p/arxmliv/no_problems_broken"
 echo "Identify files smaller than 40 bytes."
 
 # requesting all files that are smaller than 40 bytes -> most likely broken
-files=$(find $INPUT/ -maxdepth 1 -type f -size -40c -printf "%P ")
+files=$(find $INPUT/ -maxdepth 1 -type f -name "*.txt" -size -40c -printf "%P ")
 
 echo "Identified files... start moving them to $OUTPUT/"
 
 for file in $files; do
 	# get the name without path and file extensions
 	name=${file%.*}
+	echo "- move $name"
+	# mv from train (txt)
 	$(mv $INPUT/$name.txt $OUTPUT/$name.txt)
+	# mv from raw (html)
 	$(mv $RAW/$name.html $OUTPUT/$name.html)
+	# mv from txt (txt | ann)
 	$(mv $TXT/$name.* $OUTPUT/txt/)
 done
 
