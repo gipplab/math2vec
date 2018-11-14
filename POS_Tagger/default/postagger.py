461f139088bcef232cba1cd867d616ab605c6b81
Terry Ruas <truas@umich.edu>
2018-11-14 16:33:45 -0500

    postagger.py use file path in command line for exection

--- POS_Tagger/default/postagger.py
+++ POS_Tagger/default/postagger.py
@@ -26,29 +26,31 @@ logging.basicConfig(
     level=logging.INFO)
 
 #main program
-if __name__ == '__main__':  
-    #===========================================================================
-    # #IF you want to use COMMAND LINE for folder path
-    # parser = argparse.ArgumentParser(description="POS_Tagger - Generic Tagger for Text Documents")
-    # parser.add_argument('--info', type=str, help='<files/> folder should be at the same level as default package')
-    # parser.add_argument('--input', type=str, action='store', dest='inf', metavar='<folder>', required=True, help='input folder to read document(s)')
-    # parser.add_argument('--output', type=str, action='store', dest='ouf', metavar='<folder>', required=True, help='output folder to write document(s)')
-    # args = parser.parse_args()   
-    #  
-    # #COMMAND LINE  folder paths
-    # input_folder = args.inf #dest=inf
-    # output_folder = args.ouf #dest=ouf
-    #  
-    # #in/ou relative location - #input/output/model folders are under synset/module/
-    # in_foname = os.path.join(ppydir_name, input_folder) 
-    # ou_foname = os.path.join(ppydir_name, output_folder)
-    #===========================================================================
+if __name__ == '__main__': 
+     
+    #IF you want to use COMMAND LINE for folder path
+    parser = argparse.ArgumentParser(description="POS_Tagger - Generic Tagger for Text Documents")
+    parser.add_argument('--info', type=str, help='<files/> folder should be at the same level as default package')
+    parser.add_argument('--input', type=str, action='store', dest='inf', metavar='<folder>', required=True, help='input folder to read document(s)')
+    parser.add_argument('--output', type=str, action='store', dest='ouf', metavar='<folder>', required=True, help='output folder to write document(s)')
+    args = parser.parse_args()   
+      
+    #COMMAND LINE  folder paths
+    input_folder = args.inf #dest=inf
+    output_folder = args.ouf #dest=ouf
+      
+    #in/ou relative location - #input/output/model folders are under synset/module/
+    in_foname = os.path.join(ppydir_name, input_folder) 
+    ou_foname = os.path.join(ppydir_name, output_folder)
     
     #===========================================================================
     # IDE - Path Definitions
     #===========================================================================
-    in_foname = 'C:/Users/terry/Documents/Programming/eclipse-workspace/POS_Tagger/files/input/d3'
-    ou_foname = 'C:/Users/terry/Documents/Programming/eclipse-workspace/POS_Tagger/files/output'
+    #===========================================================================
+    # in_foname = 'C:/Users/terry/Documents/Programming/eclipse-workspace/POS_Tagger/files/input/d3'
+    # ou_foname = 'C:/Users/terry/Documents/Programming/eclipse-workspace/POS_Tagger/files/output'
+    # 
+    #===========================================================================
     
     #Doc and File list
     doc_paths =  fm.doclist_multifolder(in_foname)
